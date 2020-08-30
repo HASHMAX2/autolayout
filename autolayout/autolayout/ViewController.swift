db6e0576689ed2666e6409491a9b286ffd2377b9
ali hashmi <alihashmi@MacBook-Pro.local>
2020-08-30 17:38:08 +0530

    inital chnages t

---
+++ autolayout/autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by ali hashmi on 30/08/20.
+//  Copyright © 2020 ali hashmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
